bffac9fad5df590e6582ee76b837e56ae968feae
Samuel Goyes <Samuel.goyes@outlook.com>
2023-10-23 16:41:08 +0200

    HA 1.1

--- src/pgdp/hello/PracticalCourse.java
+++ src/pgdp/hello/PracticalCourse.java
@@ -2,6 +2,6 @@ package pgdp.hello;
 
 public class PracticalCourse {
     public static void main(String[] args) {
-        System.out.println("Ich freue mich übermäßig auf PGdP!");
+        System.out.println("Ich freue mich übermäßig auf PGdP!!");
     }
 }
